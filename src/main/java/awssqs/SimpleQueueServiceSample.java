4d4dbc07da444b798a8fbeca85c10ade89960aeb
rjirwanshah rjhassan <rjirwanshah@rjirwanshah.local>
2016-11-01 13:11:22 +0800

    aws sqs using java

---
+++ src/main/java/awssqs/SimpleQueueServiceSample.java
@@ -0,0 +1,68 @@
+package awssqs;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+
+public class SimpleQueueServiceSample {
+	
+	public static void main(String[] args) throws Exception {
+		
+		AWSCredentials credentials = null;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                    "Please make sure that your credentials file is at the correct " +
+                    "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+        
+        AmazonSQS sqs = new AmazonSQSClient(credentials);
+        Region usEast2 = Region.getRegion(Regions.US_EAST_1);
+        sqs.setRegion(usEast2);
+
+        System.out.println("===========================================");
+        System.out.println("Getting Started with Amazon SQS");
+        System.out.println("===========================================\n");
+        
+        try {
+            // Create a queue
+            System.out.println("Creating a new SQS queue called MyQueue.\n");
+            CreateQueueRequest createQueueRequest = new CreateQueueRequest("MyQueue");
+            String myQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+
+            // List queues
+            System.out.println("Listing all queues in your account.\n");
+            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+                System.out.println("  QueueUrl: " + queueUrl);
+            }
+            System.out.println();
+            
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it " +
+                    "to Amazon SQS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered " +
+                    "a serious internal problem while trying to communicate with SQS, such as not " +
+                    "being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+
+
+		
+	}
+
+}
